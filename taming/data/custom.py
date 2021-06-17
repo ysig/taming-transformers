7f92b120f97c3a0a0af0ef957d4baded4e5f6b9c
ysig <y.siglidis@gmail.com>
2021-06-17 21:18:00 +0300

    fix

--- taming/data/custom.py
+++ taming/data/custom.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms, io
 import sys
 import random
+import albumentations
 
 def rdir(rootdir):
     for folder, _, files in os.walk(rootdir):
@@ -20,18 +21,33 @@ class CustomTrain(Dataset):
         random.seed(0)
         random.shuffle(self.data)
         self.data = self.data[:-100]
+        self.coord = coord
+        if crop_size is not None:
+            self.cropper = albumentations.RandomCrop(height=crop_size,width=crop_size)
+            if self.coord:
+                self.cropper = albumentations.Compose([self.cropper],
+                                                      additional_targets={"coord": "image"})
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, i):
-        ex = self.data[i]
-        img = io.read_image(ex)
-        ex["image"] = self.transform(img)
+        ex = {}
+        ex["image"] = io.read_image(self.data[i])
+        if hasattr(self, "cropper"):
+            if not self.coord:
+                out = self.cropper(image=ex["image"])
+                ex["image"] = out["image"]
+            else:
+                h,w,_ = ex["image"].shape
+                coord = np.arange(h*w).reshape(h,w,1)/(h*w)
+                out = self.cropper(image=ex["image"], coord=coord)
+                ex["image"] = out["image"]
+                ex["coord"] = out["coord"]
         ex["class"] = 0
         return ex
 
-class CustomTrain(Dataset):
+class CustomVal(Dataset):
     # CelebAHQ [0] + FFHQ [1]
     def __init__(self, size, dir, keys=None, crop_size=None, coord=False):
         self.transform = transforms.Compose([transforms.Resize(size)])
@@ -39,13 +55,28 @@ class CustomTrain(Dataset):
         random.seed(0)
         random.shuffle(self.data)
         self.data = self.data[-100:]
+        self.coord = coord
+        if crop_size is not None:
+            self.cropper = albumentations.RandomCrop(height=crop_size,width=crop_size)
+            if self.coord:
+                self.cropper = albumentations.Compose([self.cropper],
+                                                      additional_targets={"coord": "image"})
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, i):
-        ex = self.data[i]
-        img = io.read_image(ex)
-        ex["image"] = self.transform(img)
+        ex = {}
+        ex["image"] = io.read_image(self.data[i])
+        if hasattr(self, "cropper"):
+            if not self.coord:
+                out = self.cropper(image=ex["image"])
+                ex["image"] = out["image"]
+            else:
+                h,w,_ = ex["image"].shape
+                coord = np.arange(h*w).reshape(h,w,1)/(h*w)
+                out = self.cropper(image=ex["image"], coord=coord)
+                ex["image"] = out["image"]
+                ex["coord"] = out["coord"]
         ex["class"] = 0
         return ex
